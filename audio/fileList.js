36595223618aab492b356830759eada11131bb26
YangH9 <49155616+YangH9@users.noreply.github.com>
2023-12-05 16:18:44 +0800

    [update]自动提交

--- audio/fileList.js
+++ audio/fileList.js
@@ -1,8 +1,10 @@
 callback({
   list: [
     { name: '海底-凤凰传奇.mp3' },
+    { name: '海底-凤凰传奇.flac' },
     { name: '全是爱-凤凰传奇.mp3' },
     { name: '山河图-凤凰传奇.mp3' },
+    { name: '山河图-凤凰传奇.flac' },
     { name: '奢香夫人-凤凰传奇.mp3' },
     { name: '奢香夫人-凤凰传奇.flac' }
   ]
