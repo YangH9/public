8e77067f4f48c873804aea210284caae951c24dd
Yang <49155616+YangH9@users.noreply.github.com>
2023-12-07 11:07:19 +0800

    [update]自动提交

--- audio/fileList.js
+++ audio/fileList.js
@@ -1,11 +1,14 @@
 callback({
-  list: [
+  allList: [
     { name: '海底-凤凰传奇.mp3' },
-    { name: '海底-凤凰传奇.flac' },
     { name: '全是爱-凤凰传奇.mp3' },
     { name: '山河图-凤凰传奇.mp3' },
-    { name: '山河图-凤凰传奇.flac' },
-    { name: '奢香夫人-凤凰传奇.mp3' },
-    { name: '奢香夫人-凤凰传奇.flac' }
+    { name: '奢香夫人-凤凰传奇.mp3' }
+  ],
+  fhcqList: [
+    { name: '海底-凤凰传奇.mp3' },
+    { name: '全是爱-凤凰传奇.mp3' },
+    { name: '山河图-凤凰传奇.mp3' },
+    { name: '奢香夫人-凤凰传奇.mp3' }
   ]
 })
