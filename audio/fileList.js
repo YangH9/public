37a509eaab78ad5eac98903b41a99d75f772c72a
YangH9 <49155616+YangH9@users.noreply.github.com>
2023-12-07 16:40:08 +0800

    [update]自动提交

--- audio/fileList.js
+++ audio/fileList.js
@@ -1,14 +1,22 @@
 callback({
   allList: [
-    { name: '海底-凤凰传奇.mp3' },
-    { name: '全是爱-凤凰传奇.mp3' },
-    { name: '山河图-凤凰传奇.mp3' },
-    { name: '奢香夫人-凤凰传奇.mp3' }
+    { name: '海底-凤凰传奇', type: ['mp3', 'flac'] },
+    { name: '全是爱-凤凰传奇', type: ['mp3'] },
+    { name: '山河图-凤凰传奇', type: ['mp3', 'flac'] },
+    { name: '奢香夫人-凤凰传奇', type: ['mp3', 'flac'] },
+    { name: '荷塘月色-凤凰传奇', type: ['mp3', 'flac'] },
+    { name: '月亮之上-凤凰传奇', type: ['mp3', 'flac'] },
+    { name: '最炫民族风-凤凰传奇', type: ['mp3', 'flac'] },
+    { name: '自由飞翔-凤凰传奇', type: ['mp3', 'flac'] }
   ],
   fhcqList: [
-    { name: '海底-凤凰传奇.mp3' },
-    { name: '全是爱-凤凰传奇.mp3' },
-    { name: '山河图-凤凰传奇.mp3' },
-    { name: '奢香夫人-凤凰传奇.mp3' }
+    { name: '海底-凤凰传奇', type: ['mp3', 'flac'] },
+    { name: '全是爱-凤凰传奇', type: ['mp3'] },
+    { name: '山河图-凤凰传奇', type: ['mp3', 'flac'] },
+    { name: '奢香夫人-凤凰传奇', type: ['mp3', 'flac'] },
+    { name: '荷塘月色-凤凰传奇', type: ['mp3', 'flac'] },
+    { name: '月亮之上-凤凰传奇', type: ['mp3', 'flac'] },
+    { name: '最炫民族风-凤凰传奇', type: ['mp3', 'flac'] },
+    { name: '自由飞翔-凤凰传奇', type: ['mp3', 'flac'] }
   ]
 })
