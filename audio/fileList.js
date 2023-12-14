c90600647c42f4b44b81ea6cadd7c9b02297d82a
Yang <49155616+YangH9@users.noreply.github.com>
2023-12-14 16:39:33 +0800

    [update]自动提交

--- audio/fileList.js
+++ audio/fileList.js
@@ -20,5 +20,13 @@ callback({
     { name: '自由飞翔-凤凰传奇', type: ['mp3', 'flac'] },
     { name: '我从草原来-凤凰传奇', type: ['mp3', 'flac'] },
     { name: '狼的诱惑-凤凰传奇', type: ['mp3', 'flac'] },
+    { name: '自由自在-凤凰传奇', type: ['mp3', 'flac'] },
+    { name: '等爱的玫瑰-凤凰传奇', type: ['mp3', 'flac'] },
+    { name: '将进酒-凤凰传奇', type: ['mp3', 'flac'] },
+    { name: '我和草原有个约定-凤凰传奇', type: ['mp3', 'flac'] },
+    { name: '天蓝蓝-凤凰传奇', type: ['mp3', 'flac'] },
+    { name: '策马奔腾-凤凰传奇', type: ['mp3', 'flac'] },
+    { name: '我们的歌谣-凤凰传奇', type: ['mp3', 'flac'] },
+    { name: '天籁传奇-凤凰传奇', type: ['mp3', 'flac'] }
   ]
 })
